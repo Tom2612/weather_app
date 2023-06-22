cc4c277674d40a6b0c8d03aba3d7977f09c06945
tom <tomcat-26@hotmail.co.uk>
2023-06-22 12:32:15 +0800

    Added localstorage usage and message if no location present

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SearchForm from './components/SearchForm';
 import CityList from './components/CityList';
 import WeatherDisplay from './components/WeatherDisplay';
@@ -8,6 +8,27 @@ import ExtendedWeatherDisplay from './components/ExtendedWeatherDisplay';
 function App() {
   const [cities, setCities] = useState(null);
   const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const getPreviousLocation = async () => {
+      const location = localStorage.getItem('location');
+      if (!location) {
+        return;
+      } else {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location.split(',')[0]},${location.split(',')[1]}&appid=${import.meta.env.VITE_app_ID}&units=metric`);
+        const response2 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location.split(',')[0]},${location.split(',')[1]}&appid=${import.meta.env.VITE_app_ID}&units=metric`);
+        const data = await response.json();
+        const data2 = await response2.json();
+        setData({
+          weather: data,
+          forecast: [data2.list[2], data2.list[10], data2.list[18], data2.list[26], data2.list[34]]
+        });
+      }
+    };
+
+    getPreviousLocation();
+
+  }, [])
 
   const handleSelect = async (city) => {
 
@@ -23,6 +44,7 @@ function App() {
       weather: weatherjson,
       forecast: [forecastjson.list[2], forecastjson.list[10], forecastjson.list[18], forecastjson.list[26], forecastjson.list[34]]
     });
+    localStorage.setItem('location', city.name + ',' + city.country);
   }
 
   return (
@@ -32,8 +54,9 @@ function App() {
           <SearchForm setCities={setCities} />
           {cities && <CityList list={cities} handleSelect={handleSelect} /> }
         </div>
-      
+
         <div id='weather-container'>
+          {!data && <h1 style={{textAlign: 'center', width: '100%'}}>Start searching</h1>}
           {data && <WeatherDisplay data={data} /> }
           {data && <ExtendedWeatherDisplay data={data} />}
         </div>
